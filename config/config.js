a48b4f63dc6e9863878d030b10d75adbb0b3635e
juwitaw <juwita.wina@yahoo.com>
2020-01-01 22:45:10 +0700

    inisialisasi repo

---
+++ config/config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    // ubah DB dengan host mongodb masing-masing
+    DB: process.env.HOST ? process.env.HOST : 'mongodb+srv://admin:Sementara3@learnit-sandbox-5c8vm.mongodb.net/learnit-react?retryWrites=true&w=majority',
+    APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 80,
+  };
