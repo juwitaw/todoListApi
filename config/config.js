7d16e1854c655ac6e738556876cc31838d3e016d
juwitaw <juwita.wina@yahoo.com>
2020-01-02 13:34:26 +0700

    change env APP_PORT to PORT

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@
 module.exports = {
     // ubah DB dengan host mongodb masing-masing
     DB: process.env.HOST ? process.env.HOST : 'mongodb+srv://admin:Sementara3@learnit-sandbox-5c8vm.mongodb.net/learnit-react?retryWrites=true&w=majority',
-    APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 80,
+    APP_PORT: process.env.PORT ? process.env.PORT : 80,
   };
